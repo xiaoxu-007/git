c0873a0564522bf524224693267ed35d95ab251e
xiaoxu <xiaoxu@githao.com>
2022-03-18 17:15:33 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-03-18 17:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
