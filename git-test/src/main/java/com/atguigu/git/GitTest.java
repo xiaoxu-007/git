c23f487a10d835a2a05930c0ec3898fb14bf63c5
xiaoxu <xiaoxu@githao.com>
2022-03-18 19:26:37 +0800

    master test

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hot-fix hello git4!");
+        System.out.println("111111");
     }
 }
